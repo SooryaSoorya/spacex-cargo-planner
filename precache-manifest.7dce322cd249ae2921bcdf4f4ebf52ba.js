c4f3cc00a01512eb5b42c1555f6051ce61eb4a24
SooryaSoorya <haisoorya.s@gmail.com>
2020-08-18 20:44:42 +0300

    Updates

---
+++ precache-manifest.7dce322cd249ae2921bcdf4f4ebf52ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25aababb7be2653d9e7a800227e3792d",
+    "url": "/spacex-cargo-planner/index.html"
+  },
+  {
+    "revision": "a7301b08dd093109737f",
+    "url": "/spacex-cargo-planner/static/css/main.438c917f.chunk.css"
+  },
+  {
+    "revision": "46431e704204bc6196b1",
+    "url": "/spacex-cargo-planner/static/js/2.c74ce971.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spacex-cargo-planner/static/js/2.c74ce971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7301b08dd093109737f",
+    "url": "/spacex-cargo-planner/static/js/main.a09a0c73.chunk.js"
+  },
+  {
+    "revision": "f561eb98a8abe3c8ecfa",
+    "url": "/spacex-cargo-planner/static/js/runtime-main.ed80517e.js"
+  }
+]);
